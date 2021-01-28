a2825b2f53c2eeee23c1c7d9778c2480c10ba6a7
Arpan patel <arpan.a.patel@capgemini.com>
2021-01-28 16:12:09 +0530

    First Commit

---
+++ src/codeql-example/codeql-example/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace codeql_example
+{
+    class Program
+    {
+        public static string str = "adfwaefrSSS#3444F^%^&FFFF";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
